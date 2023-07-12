aa27681374e0bf8863542232d7acf7dc706f0299
JK13xyz <jens@jk13.dev>
2023-07-12 18:05:01 +0200

    Added sops and age to Flake for future use

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
       
     };
 
-  outputs = inputs@{ self, lib, nixpkgs, nixpkgs-stable, home-manager, hyprland, ... }: 
+  outputs = inputs@{ self, lib, nixpkgs, nixpkgs-stable, home-manager, hyprland, sops-nix ... }: 
     let
       system = "x86_64-linux";
       username = "jens";
