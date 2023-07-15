6ed9142e4768011c9162b054031464cf94a489fa
JK13xyz <jens@jk13.dev>
2023-07-16 00:21:32 +0200

    Added options for ZSH

--- home.nix
+++ home.nix
@@ -30,6 +30,9 @@
 
   programs.zsh = {
     enable = true;
+    enableCompletion = true;
+    enableAutosuggestions = true;
+    enableSyntaxHighlighting = true;
     oh-my-zsh = {
       enable = true;
       plugins = [ "git" ];
