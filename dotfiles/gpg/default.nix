d7c0c93760fbff08d992c79a18505161deedf0f5
JK13xyz <jens@jk13.dev>
2023-07-16 18:01:58 +0200

    Minor change

--- dotfiles/gpg/default.nix
+++ dotfiles/gpg/default.nix
@@ -8,12 +8,10 @@
     ];
     settings = {
         default-key = "C577 B6C5 56EF 76FA 78E1  B271 E2AF F28A B89D 48EC";
-        default-recepient-self = true;
         auto-key-locate = "local,wkd,keyserver";
         keyserver = "hkps://keys.openpgp.org";
         auto-key-retrieve = true;
         auto-key-import = true;
-        keyserver-option = "honor-keyserver-url";
     };
   };
 
