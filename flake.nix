1b06e4f11fff10b10be84883221803255f5f5269
JK13xyz <jens@jk13.dev>
2023-07-16 17:17:48 +0200

    More restructuring

--- flake.nix
+++ flake.nix
@@ -52,7 +52,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.jens = import ./home.nix;
+              home-manager.users.jens = import ./home/jens/home.nix;
               }
           ];
          };
---
+++ home/jens/home.nix
@@ -0,0 +1,40 @@
+{ config, lib, pkgs, inputs, ... }:
+
+{
+  home = {
+    username = lib.mkDefault "jens";
+    homeDirectory = lib.mkDefault "/home/${config.home.username}";
+    packages = with pkgs; [
+      pkgs.ncspot
+    ];
+    stateVersion = lib.mkDefault "23.05";
+    sessionVariables = {
+      XDG_RUNTIME_DIR = "/run/user/$UID";
+    };
+  };
+
+  programs.home-manager.enable = true;
+
+  imports =
+    [ 
+      ./dotfiles/git/default.nix
+      ./dotfiles/firefox/default.nix
+      ./dotfiles/neofetch/default.nix
+      ./dotfiles/starship/default.nix
+      ./dotfiles/waybar/default.nix
+      ./dotfiles/zsh/default.nix
+    ];  
+
+  services.clipman = {
+    enable = true;
+  };
+
+  services.flameshot = {
+    enable = true;
+  };
+
+  services.nextcloud-client = {
+    enable = true;
+    startInBackground = true;
+  };
+}
