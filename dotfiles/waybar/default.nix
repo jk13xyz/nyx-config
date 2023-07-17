85c9cc37b59dac69234e7614530a546d08fcda8c
JK13xyz <jens@jk13.dev>
2023-07-17 17:31:52 +0200

    Some uncommited changes

--- dotfiles/waybar/default.nix
+++ dotfiles/waybar/default.nix
@@ -5,7 +5,7 @@
     enable = true;
   };
 
-home.file.".config/waybar/style.css".source = ./style.css;
-home.file.".config/waybar/config.jsonc".source = ./config.jsonc;
+  home.file.".config/waybar/style.css".source = ./style.css;
+  home.file.".config/waybar/config.jsonc".source = ./config.jsonc;
 
  }
