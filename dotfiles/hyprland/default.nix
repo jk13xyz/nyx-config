bb24051e9c058d808855c6a9d759eeb12765cc9a
JK13xyz <jens@jk13.dev>
2023-07-17 13:14:05 +0200

    Fix for hyprland

--- dotfiles/hyprland/default.nix
+++ dotfiles/hyprland/default.nix
@@ -1,14 +1,6 @@
-{config, pkgs, ...}:
+{ config, pkgs, ... }:
 {  
-    home = {
-        packages = with pkgs; [
-        pkgs.hyprland
-        ];
-    };
-
-    programs.hyprland = {
+    programs.hyprland= {
         enable = true;
     };
-
-    #  home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
 }
