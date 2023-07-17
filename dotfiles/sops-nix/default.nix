c38c0b4f758fecf5d14926805e668c7efdd20f86
JK13xyz <jens@jk13.dev>
2023-07-17 18:00:31 +0200

    Playing with sops-nix

--- dotfiles/sops-nix/default.nix
+++ dotfiles/sops-nix/default.nix
@@ -1,3 +1,12 @@
 {
-    home.file."${config.xdg.configHome}/.sops.yaml".source = ${config.xdg.configHome}/.sops.yaml;
+    home.file."${config.xdg.configHome}/.sops.yaml".text = ''
+        keys:
+        - &user_jens_gpg C577B6C556EF76FA78E1B271E2AFF28AB89D48EC
+        creation_rules:
+        - path_regex: secrets/[^/]+\.yaml$
+            key_groups:
+            - pgp:
+                - *user_jens_gpg
+                age:
+    '';
 }
