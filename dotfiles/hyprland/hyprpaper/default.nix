84b15cbfd7f1fadd8f97f42043b61a8699dd0459
JK13xyz <jens@jk13.dev>
2023-07-17 13:00:05 +0200

    Changed hyprpaper.nix

---
+++ dotfiles/hyprland/hyprpaper/default.nix
@@ -0,0 +1,10 @@
+{
+    home = {
+        packages = with pkgs; [
+        pkgs.hyprpaper
+        ];
+    };
+
+    home.file.".config/hypr/hyprpaper.conf".source = ./hyprpaper.conf;
+    home.file.".config/hypr/wallpaper.jpg".source = ./wallpaper.jpg;
+}
