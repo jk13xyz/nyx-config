ce693fe07cdcadddd48d44ed671085007a48de9d
JK13xyz <jens@jk13.dev>
2023-07-19 18:32:41 +0200

    Turns out, you can't remove too much or it breaks

--- flake.nix
+++ flake.nix
@@ -50,6 +50,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
+              home-manager.users.jens = import ./home/jens/home.nix;
             }
           ];
         };
