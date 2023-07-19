66a75f947d74506b0970778999a7552d1cbf679c
JK13xyz <jens@jk13.dev>
2023-07-19 15:03:14 +0200

    Typofix

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,5 +1,5 @@
 { config, ... }:
 {
     home.file."./.sops.yaml".source = ./.sops.yaml;
-    home.file."./secrets.yaml".source = ./secrets.yaml;
+    home.file."./secrets.yaml".source = ./secrets/secrets.yaml;
 }
