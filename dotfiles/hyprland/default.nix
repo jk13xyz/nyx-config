1eaab3c22d1a5db0895751a18627ff1ccf12b248
JK13xyz <jens@jk13.dev>
2023-07-20 12:59:52 +0200

    Minor change for Hyprland

--- dotfiles/hyprland/default.nix
+++ dotfiles/hyprland/default.nix
@@ -2,5 +2,11 @@
 {  
     programs.hyprland = {
         enable = true;
+        xwayland = true;
+    };
+
+    environment.sessionVariables = {
+        WLR_NO_HARDWARE_CURSOR = "1";
+        NIXOS_OZONE_WL = "1";
     };
 }
