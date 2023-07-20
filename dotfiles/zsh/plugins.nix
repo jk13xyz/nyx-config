65782f7565391fb28b1048c8ea8530fe7e7d335a
JK13xyz <jens@jk13.dev>
2023-07-20 18:48:23 +0200

    Change to zsh conf

--- dotfiles/zsh/plugins.nix
+++ dotfiles/zsh/plugins.nix
@@ -1,9 +1,11 @@
 {
-    plugins = [
-            {
-              name = "powerlevel10k";
-              src = pkgs.zsh-powerlevel10k;
-              file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-            }
-    ];
+    programs.zsh = {
+        plugins = [
+                {
+                name = "powerlevel10k";
+                src = pkgs.zsh-powerlevel10k;
+                file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+                }
+        ];
+    };
 }
