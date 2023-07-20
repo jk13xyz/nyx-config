248c7b039905a6b87e3a27127106dbb19a1f9714
JK13xyz <jens@jk13.dev>
2023-07-20 17:49:50 +0200

    Added p10k

--- dotfiles/zsh/default.nix
+++ dotfiles/zsh/default.nix
@@ -13,6 +13,15 @@
             plugins = [ "git"  ];
         };
 
+        plugins = [
+            {
+                name = "powerlevel10k";
+                src = pkgs.zsh-powerlevel10k;
+                file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+            }
+
+        ];
+
         shellAliases = {
             # General
             c = "clear";
