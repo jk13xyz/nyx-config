66e61859b35a799ae3c51224bad5c8ca8c886b77
JK13xyz <jens@jk13.dev>
2023-07-21 10:44:36 +0200

    Added wireless.nix

---
+++ hosts/common/wireless.nix
@@ -0,0 +1,7 @@
+{
+    sops.secrets."wireless.env" = { }; 
+    networking.wireless = {
+        environmentFile = config.sops.secrets."wireless.env".path; 
+        networks = { "@home_uuid@" = { psk = "@home_psk@"; }; };
+    }
+}
