df8e24b2f48040d60f4ac1953c6e6d355a856a3b
JK13xyz <jens@jk13.dev>
2023-07-21 13:04:16 +0200

    Fix for secrets

--- hosts/nyx-pad/configuration.nix
+++ hosts/nyx-pad/configuration.nix
@@ -67,7 +67,7 @@
   sops.defaultSopsFile = ../../secrets/nyx-pad.yaml;
   sops.age.sshKeyPaths = [ "/home/jens/.ssh/id_ed25519" ];
   sops.age.keyFile = "/home/jens/.config/sops/age/keys.txt";
-  sops.secrets."user/jens/passwd" = { sopsFile = ../../secrets/secrets.yaml; };
+  sops.secrets."user/jens/passwd" = { sopsFile = ../../secrets/nyx-pad.yaml; };
 
   system.stateVersion = "23.05";
 }
