299811d4a5305767598316d420710dc076f46dbe
JK13xyz <jens@jk13.dev>
2023-07-21 13:02:29 +0200

    Minor fix

--- hosts/nyx-pad/configuration.nix
+++ hosts/nyx-pad/configuration.nix
@@ -64,7 +64,7 @@
     yubikey-manager
   ];
 
-  sops.defaultSopsFile = ../../secrets/secrets.yaml;
+  sops.defaultSopsFile = ../../secrets/nyx-pad.yaml;
   sops.age.sshKeyPaths = [ "/home/jens/.ssh/id_ed25519" ];
   sops.age.keyFile = "/home/jens/.config/sops/age/keys.txt";
   sops.secrets."user/jens/passwd" = { sopsFile = ../../secrets/secrets.yaml; };
