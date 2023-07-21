57137ab55348a6262fed3b89fe9e663ebaa6e694
JK13xyz <jens@jk13.dev>
2023-07-21 17:35:35 +0200

    More improvements

--- decrypt-key.sh
+++ decrypt-key.sh
@@ -9,13 +9,6 @@ do
         p)  
             persist=${OPTARG}      # If "y" or "yes", keys are written to $HOME
             ;;
-        help)  
-            echo "Usage: ./decrypt-keys.sh [-h host_name] [-p persist]"
-            echo "Host name should equal the file name of your sops file"
-            echo "Persist writes age key to $HOME if 'y' or 'yes' is passed"
-            echo "Example: ./decrypt-keys.sh -h host -p yes"
-            exit 1
-            ;;
     esac
 done
 
@@ -24,32 +17,53 @@ tmpDir=/tmp/keys
 
 mkdir -p $tmpDir
 
-echo $homeDir
-echo $tmpDir
+if [ -z "$hostname" ];
 
-nix-shell -p gnupg --run '\
-    gpg \
-    --keyserver hkps://keys.openpgp.org \
-    --recv-keys C577B6C556EF76FA78E1B271E2AFF28AB89D48EC &&
-    gpg \
-    --decrypt ./secrets/'${hostname}'.gpg \
-    > '$tmpDir'/id_ed25519 \
-    '
+then
+    echo "You did not pass a host name. Please try again."
+    echo
+    echo "Usage: ./decrypt-keys.sh [-h host_name] [-p persist]"
+    echo "Host name should equal the file name of your sops file"
+    echo "Persist writes age key to $HOME if 'y' or 'yes' is passed"
+    echo "Example: ./decrypt-keys.sh -h host -p yes"
+    exit 1
 
-if [ $persist='y' ] || [ $persist='yes' ];
+else
+
+    if [ -f './secrets/'$hostname'.gpg' ];
 
     then
-        nix-shell -p ssh-to-age --run '\
-            ssh-to-age \
-                -private-key \
-                -i '$tmpDir'/id_ed25519 \
-                -o '$homeDir'/keys.txt \
+        nix-shell -p gnupg --run '\
+            gpg \
+            --keyserver hkps://keys.openpgp.org \
+            --recv-keys C577B6C556EF76FA78E1B271E2AFF28AB89D48EC &&
+            gpg \
+            --decrypt ./secrets/'${hostname}'.gpg \
+            > '$tmpDir'/id_ed25519 \
             '
+
+        if [ $persist='y' ] || [ $persist='yes' ];
+
+            then
+                nix-shell -p ssh-to-age --run '\
+                    ssh-to-age \
+                        -private-key \
+                        -i '$tmpDir'/id_ed25519 \
+                        -o '$homeDir'/keys.txt \
+                    '
+            else
+                nix-shell -p ssh-to-age --run '\
+                    ssh-to-age \
+                        -private-key \
+                        -i '$tmpDir'/id_ed25519 \
+                        -o '$tmpDir'/keys.txt \
+                '        
+        fi
+
     else
-        nix-shell -p ssh-to-age --run '\
-            ssh-to-age \
-                -private-key \
-                -i '$tmpDir'/id_ed25519 \
-                -o '$tmpDir'/keys.txt \
-        '        
+        echo "File ./secrets'$hostname'.gpg' was not found."
+        exit 1
+    
+    fi
+
 fi
