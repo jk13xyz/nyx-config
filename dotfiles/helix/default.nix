eff4b42501d66e31dcbdf8cde964e4e3ceac2009
JK13xyz <jens@jk13.dev>
2023-07-21 16:05:08 +0200

    Added helix for testing

---
+++ dotfiles/helix/default.nix
@@ -0,0 +1,17 @@
+{
+  programs.helix = {
+    enable = true;
+    settings = {
+      editor = {
+        color-modes = true;
+        line-number = "relative";
+        indent-guides.render = true;
+        cursor-shape = {
+          normal = "block";
+          insert = "bar";
+          select = "underline";
+        };
+      };
+    };
+  };
+}
