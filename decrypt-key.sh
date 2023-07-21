6a96533baf002e10928bdef9862a9714fff725c6
JK13xyz <jens@jk13.dev>
2023-07-21 17:09:15 +0200

    Some more changing for decrypt-keys.sh

--- decrypt-key.sh
+++ decrypt-key.sh
@@ -1,40 +1,48 @@
 # !/usr/bin/env bash
 
-while getopts h:p: flag
+while getopts ":h:p:" option
 do
-    case "${flag}" in
+    case $option in
         h) hostname=${OPTARG};;     # Queries the host-name
         p) persist=${OPTARG};;      # If "y" or "yes", keys are written to $HOME
+	*) echo "Usage: ./decrypt-keys.sh [-h host_name] [-p persist]"
+	   echo "Host name should equal the file name of your sops file"
+	   echo "Persist writes age key to $HOME if 'y' or 'yes' is passed"
+	   echo "Example: ./decrypt-keys.sh -h host -p yes";;
     esac
 done
 
-homeDir = "$HOME"
-tmpDir = "/tmp/keys"
+homeDir=$HOME
+tmpDir=/tmp/keys
+
+mkdir -p ${tmpDir}
+
+echo $homeDir
+echo $tmpDir
 
 nix-shell -p gnupg --run '\
-    mkdir -p ${tmpDir} &&
     gpg \
     --keyserver hkps://keys.openpgp.org \
     --recv-keys C577B6C556EF76FA78E1B271E2AFF28AB89D48EC &&
     gpg \
     --decrypt ./secrets/'${hostname}'.gpg \
-    > ${tmpDir}/id_ed25519 \
+    > $tmpDir/id_ed25519 \
     '
 
-if [ ${persist} == "y" ] || [ ${persist} = "yes" ];
+if [ ${persist} = "y" ] || [ ${persist} = "yes" ];
 
     then
         nix-shell -p ssh-to-age --run '\
             ssh-to-age \
                 -private-key \
-                -i ${tmpDir}/id_ed25519 \
-                -o ${homeDir}/keys.txt \
+                -i $tmpDir/id_ed25519 \
+                -o $homeDir/keys.txt \
             '
     else
         nix-shell -p ssh-to-age --run '\
             ssh-to-age \
                 -private-key \
-                -i ${tmpDir}/id_ed25519 \
-                -o ${tmpDir}/keys.txt \
+                -i $tmpDir/id_ed25519 \
+                -o $tmpDir/keys.txt \
         '        
 fi
