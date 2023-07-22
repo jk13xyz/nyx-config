416782eb0c63dd5fa9b0f33edce2bfc7625b63f0
JK13xyz <jens@jk13.dev>
2023-07-22 13:28:17 +0200

    Setting up for secure boot

--- hosts/nyx-pad/configuration.nix
+++ hosts/nyx-pad/configuration.nix
@@ -21,15 +21,23 @@
     '';
   };
 
-  boot.loader.systemd-boot.enable = mkForce false;
-  boot.loader.systemd-boot.configurationLimit = 5;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.initrd.secrets = {
-    "/crypto_keyfile.bin" = null;
-  };
-  boot.lanzaboote = {
-    enable = true;
-    pkiBundle = "/etc/secureboot";
+  boot = {
+    loader = {
+      systemd-boot = {
+        enable = lib.mkForce false;
+        configurationLimit = 5;
+      };
+      efi = {
+        canTouchEfiVariables = true;
+      };
+      initrd = {
+        "/crypto_keyfile.bin" = null;
+      };
+      lanzaboote = {
+        enable = true;
+        pkiBundle = "/etc/secureboot";
+      };
+    };
   };
 
   networking.hostName = "nyx-pad";
