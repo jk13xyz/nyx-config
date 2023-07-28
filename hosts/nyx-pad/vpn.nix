b667aefa8c43d34fd6da19183abdb8c35002bcce
JK13xyz <jens@jk13.dev>
2023-07-28 16:58:33 +0200

    Finalised config. Works now

--- hosts/nyx-pad/vpn.nix
+++ hosts/nyx-pad/vpn.nix
@@ -2,9 +2,9 @@
 {
   networking.wg-quick.interfaces.wg0 = {
     autostart = true;
-    dns = [ 10.2.0.1 ];
+    dns = [ "10.2.0.1" ];
     privateKeyFile = config.sops.secrets.protonvpn-privkey.path;
-    address = [ 10.2.0.2/32 ];
+    address = [ "10.2.0.2/32" ];
     listenPort = 51820;
 
     peers = [
@@ -12,7 +12,7 @@
             publicKey = "Hd3hdhX18q6tnET4x77hg/xou3o/tdf7iEgLTqtRwVY=";
             allowedIPs = [ "0.0.0.0/0" ];
             endpoint = "185.159.157.14:51820";
-        };
+        }
     ];
   };
 }
